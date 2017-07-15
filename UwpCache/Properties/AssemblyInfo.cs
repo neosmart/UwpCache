33e64fe3401719ba1594553ef6727f9bf8d3d29e
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-15 15:41:00 -0500

    Bumped to version 0.2

--- UwpCache/Properties/AssemblyInfo.cs
+++ UwpCache/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UwpCache")]
-[assembly: AssemblyDescription("An object-oriented cache for your UWP applications.")]
+[assembly: AssemblyDescription("An object cache for your UWP applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NeoSmart Technologies")]
 [assembly: AssemblyProduct("UwpCache")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: ComVisible(false)]
